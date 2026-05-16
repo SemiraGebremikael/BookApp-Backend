2fb55436312710ada9fcd12ef8c6f941eb7b693e
Semira gebremikael <semiragere61@gmail.com>
2026-05-16 20:49:08 +0200

    azure deploy

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a6a9b17916056969ea5857d8a0d0a04545063e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ce2f7c26c13756dd02de5775f29294fffce471")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
