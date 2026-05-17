1041e9d46090e8aefdcc2e9f0693d8dd960a5d49
Semira gebremikael <semiragere61@gmail.com>
2026-05-18 01:36:17 +0200

    Add EF Core initial migration

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96605e300a0b4c350aeed74c67a79fb3355d4950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84544b08b614c6f1a40903afa04963676d3758ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
