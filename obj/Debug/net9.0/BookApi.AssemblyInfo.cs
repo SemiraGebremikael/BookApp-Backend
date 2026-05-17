e24a940c5fa616f5e10938497ca9756ea3cd160d
Semira gebremikael <semiragere61@gmail.com>
2026-05-18 00:11:34 +0200

    Fixed database connection issue

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ce2f7c26c13756dd02de5775f29294fffce471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12e540dc16074ed605103b9a4bd3b9e099c490a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
